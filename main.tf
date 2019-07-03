e050f40b6c5bc84e4dbe28146e9875791769f8aa
gamename <tennis.n.smith@gmail.com>
2019-07-03 15:32:35 -0500

    took out unneeded parms

--- main.tf
+++ main.tf
@@ -48,11 +48,9 @@ resource "aws_eip" "this" {
 
 module "ec2" {
   source                      = "terraform-aws-modules/ec2-instance/aws"
-  instance_count              = 1
   name                        = "training1-tf-instance"
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
-  cpu_credits                 = "unlimited"
   subnet_id                   = tolist(data.aws_subnet_ids.all.ids)[0]
   vpc_security_group_ids      = [module.security_group.this_security_group_id]
   associate_public_ip_address = true
