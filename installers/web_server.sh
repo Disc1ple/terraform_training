075da1773b7d40b60925613a8fe69c6363ba0830
gamename <tennis.n.smith@gmail.com>
2019-07-10 10:47:00 -0500

    Add instructions on what to change

--- installers/web_server.sh
+++ installers/web_server.sh
@@ -3,5 +3,6 @@ yum clean all
 yum -y update
 yum -y install httpd
 apachectl start
+# Change the line below to match your training ID
 echo "<h1>TrainingX: Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 
